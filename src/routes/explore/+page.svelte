4d194e7c9e7ec82ea0ed5cbd9ce6f81aea987347
fleetfoxx <tmporter42+fleetfoxx@gmail.com>
2023-09-13 21:52:57 -0700

    Add search cache.

--- src/routes/explore/+page.svelte
+++ src/routes/explore/+page.svelte
@@ -3,20 +3,24 @@
 
 	import SearchTermButton from './SearchTermButton.svelte';
 	import YouTube from './YouTube.svelte';
-	import type { SearchResponse, SearchTerm } from './+page.svelte';
+	import type { SearchResponse, SearchResult, SearchTerm } from './+page.svelte';
 	import { onMount } from 'svelte';
 	import { searchTerms } from './searchTerms';
 
-	const API_KEY = 'AIzaSyBlJ533t5_xnU8rYiNi-whBp9a1lCrNbjc'; // mine
+	const API_KEY = 'AIzaSyBlJ533t5_xnU8rYiNi-whBp9a1lCrNbjc';
 
-	onMount(async () => {
-		authenticate();
-		loadClient();
-		// loadNextVideo();
-	});
+	// TODO: This cache will be cleared each time the website is refreshed.
+	// Consider storing this in local/session storage.
+	const searchCache = new Map<string, SearchResponse>();
+
+	// onMount(async () => {
+	// 	authenticate();
+	// 	loadClient();
+	// 	// loadNextVideo();
+	// });
 
 	$: currentSearchTerm = searchTerms[0];
-	$: videoId = '-0kmMJ6F9WM';
+	$: videoId = '';
 
 	const handleSearchTermClick = (searchTerm: SearchTerm) => {
 		currentSearchTerm = searchTerm;
@@ -24,7 +28,7 @@
 	};
 
 	const loadNextVideo = () => {
-		return; // temporary short-circuit to preserve quota
+		// return; // temporary short-circuit to preserve quota
 		const maxResults = 50;
 		let query = currentSearchTerm.term();
 
@@ -34,28 +38,45 @@
 			query = searchTerms[randomIndex].term();
 		}
 
-		// @ts-ignore
-		return gapi.client.youtube.search
-			.list({
-				part: ['snippet'],
-				maxResults,
-				q: query,
-				type: 'video',
-				order: 'date'
-			})
-			.then(
-				function (response: { result: SearchResponse }) {
-					// Handle the results here (response.result has the parsed body).
-					console.log('Response', response);
+    console.log(searchCache);
 
-					const randomVideoIndex = Math.floor(Math.random() * (response.result.items.length - 1));
-					const randomVideoId = response.result.items[randomVideoIndex].id.videoId;
-					videoId = randomVideoId;
-				},
-				function (err: Error) {
-					console.error('Execute error', err);
-				}
-			);
+		if (searchCache.has(query)) {
+			// TODO: This has the possibility of returning the same video more than
+			// once. Consider removing the video from the cache as they are played and
+			// refilling the cache when it's empty.
+			console.log('Loading video from cache.');
+
+			const cachedSearch = searchCache.get(query)!;
+			const randomVideoIndex = Math.floor(Math.random() * (cachedSearch.items.length - 1));
+			const randomVideoId = cachedSearch.items[randomVideoIndex].id.videoId;
+			videoId = randomVideoId;
+			return;
+		} else {
+			// @ts-ignore
+			return gapi.client.youtube.search
+				.list({
+					part: ['snippet'],
+					maxResults,
+					q: query,
+					type: 'video',
+					order: 'date'
+				})
+				.then(
+					function (response: { result: SearchResponse }) {
+						// Handle the results here (response.result has the parsed body).
+						console.log('Response', response);
+
+						const randomVideoIndex = Math.floor(Math.random() * (response.result.items.length - 1));
+						const randomVideoId = response.result.items[randomVideoIndex].id.videoId;
+						videoId = randomVideoId;
+
+						searchCache.set(query, response.result);
+					},
+					function (err: Error) {
+						console.error('Execute error', err);
+					}
+				);
+		}
 	};
 
 	const authenticate = () => {
